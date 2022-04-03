1618b7abb14baf044da6540f903114037f3df913
yugmani <yugmani@yahoo.com>
2022-04-03 10:22:46 -0700

    accumulator constructor function

--- index.js
+++ index.js
@@ -176,8 +176,33 @@ function Calculator() {
   };
 }
 
-let calculator = new Calculator();
+// let calculator = new Calculator();
 
-calculator.read();
-calculator.sum();
-calculator.mul();
+// calculator.read();
+// calculator.sum();
+// calculator.mul();
+
+// Create new Accumulator
+// --------------------------------------
+
+// Create a constructor function Accumulator(startingValue).
+
+// Object that it creates should:
+
+// Store the “current value” in the property value. The starting value is set to the argument of the constructor startingValue.
+// The read() method should use prompt to read a new number and add it to value.
+// In other words, the value property is the sum of all user-entered values with the initial value startingValue.
+function Accumulator(startingValue) {
+  this.value = startingValue;
+  this.read = function () {
+    this.newValue = +prompt('Enter a value: ');
+    console.log(this.newValue);
+    this.value += this.newValue;
+    console.log(this.value);
+  };
+}
+
+let accumulator = new Accumulator(3);
+
+accumulator.read();
+accumulator.value;
