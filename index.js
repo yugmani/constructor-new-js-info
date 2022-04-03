365492d6bfd50e79dc31857e3b61632518f7511e
yugmani <yugmani@yahoo.com>
2022-04-03 08:23:55 -0700

    methods created in constructor

--- index.js
+++ index.js
@@ -70,3 +70,64 @@ function Listing1(name) {
 let johana = Listing1('Johana'); //// redirects call to new User
 console.log(johana.name); //Johana
 //This approach is sometimes used in libraries to make the syntax more flexible. So that people may call the function with or without new, and it still works.
+
+// Return from constructors
+// *****************************************
+
+//Usually, constructors do not have a `return` statement. Their task is to write all necessary stuff into `this`, and it automatically becomes the result.
+
+//But if there is a `return` statement, then the rule is simple:
+
+// (i) If return is called with an object, then the object is returned instead of `this`.
+// (ii) If return is called with a primitive, it’s ignored.
+
+//In other words, return with an object returns that object, in all other cases `this` is returned.
+function BigUser() {
+  this.name = 'John';
+
+  // return { name: 'Godzilla' }; // <-- returns this object
+  return; // <-- returns this
+}
+
+console.log(new BigUser().name); // Godzilla, got that object
+// John --> with just `return`;
+
+// *** Usually constructors don’t have a return statement.***
+
+// Omitting parentheses
+// ------------------------------------
+
+// Omitting parentheses here is not considered a “good style”
+// if constructor has no arguments, we can omit parentheses after new:
+let big1 = new BigUser(); // <-- no parentheses
+
+// big1 is same as:
+let big2 = new BigUser();
+console.log(big1); //{name: "Godzilla"}
+console.log(big2); //{name: "Godzilla"}
+
+// Methods in constructor
+// ***********************************************
+
+// The constructor function may have parameters that define how to construct the object, and what to put in it.
+
+// Of course, we can add to this not only properties, but methods as well.
+
+function MyUser(name) {
+  this.name = name;
+
+  this.sayHi = function () {
+    console.log(`My name is: ${this.name}`);
+  };
+}
+
+let myUser1 = new MyUser('James');
+myUser1.sayHi(); //My name is: James
+
+console.log(myUser1);
+/* 
+myUser1 = {
+   name: "James",
+   sayHi: function() { ... }
+}
+*/
